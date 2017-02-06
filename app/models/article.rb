e00898b4e865323b1e33bdfff21816bf59ab8211
royriver23 <roy.fita23@gmail.com>
2017-02-05 22:32:01 -0600

    master: Adding highlighting

--- app/models/article.rb
+++ app/models/article.rb
@@ -3,4 +3,25 @@ require 'elasticsearch/model'
 class Article < ApplicationRecord
   include Elasticsearch::Model
   include Elasticsearch::Model::Callbacks
+
+  def self.search(query)
+    __elasticsearch__.search(
+      {
+        query: {
+          multi_match: {
+            query: query,
+            fields: ['title', 'text']
+          }
+        },
+        highlight: {
+          pre_tags: ['<em>'],
+          post_tags: ['</em>'],
+          fields: {
+            title: {},
+            text: {}
+          }
+        }
+      }
+    )
+  end
 end
